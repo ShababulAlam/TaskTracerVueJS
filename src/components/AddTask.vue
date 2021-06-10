e10a1e3084ea9aa151b0c571762bf7e047afb39c
ShababulAlam <sababul37@gmail.com>
2021-06-10 13:24:04 +0600

    Initial Commit

---
+++ src/components/AddTask.vue
@@ -0,0 +1,64 @@
+<template>
+  <form @submit.prevent="addTaskSubmit" class="card" >
+    <div class="card-body">
+    <div class="form-group">
+      <label class="text-danger">Task Name</label>
+      <input class="form-control" type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Task Time & Date</label>
+      <input type="text" v-model="day" name="day"
+        placeholder="Add date"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default{
+    name: 'AddTask',
+    data(){
+        return{
+            text:'',
+            day:'',
+            reminder: false
+        }
+    },
+    methods:{
+        addTaskSubmit(){
+           if(!this.text){
+               alert('Please enter a task')
+               return
+           }
+           
+           const newTask = {
+               id: Math.floor(Math.random() * 100),
+               text: this.text,
+               day: this.day,
+               reminder: this.reminder,
+               
+
+           }
+           
+           this.$emit('add-task', newTask)
+           //console.log(newTask)
+           this.text=''
+           this.day=''
+           this.reminder='false'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
+
+
+
---
+++ src/components/Task.vue
@@ -0,0 +1,61 @@
+<template>
+<div @dblclick.prevent="handleTaskDBClick(task.id)" :class="[task.reminder ? 'reminder' : '', 'task', {'line-through': isLineThrough}]">
+   <h3>{{task.text}}
+       <i @click="$emit('delete-task',task.id)" class="fas fa-times-circle fa-lg"></i>
+   </h3>
+   <p>{{ task.day}} </p>
+
+</div>
+</template>
+
+<script>
+export default {
+    name:'Task',
+    data () {
+      return {
+        isLineThrough: false
+      }
+    },
+    props:{
+        task: Object
+    },
+    methods: {
+      handleTaskDBClick(taskID) {
+       
+        this.isLineThrough = !this.isLineThrough
+        this.$emit('toggle-reminder', taskID)
+      }
+    }
+    
+}
+</script>
+
+
+
+<style scoped>
+.fas {
+    display:flex;
+  color: red;
+  justify-content: flex-start;
+  
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 5px 20px;
+  cursor: pointer;
+  background: rgba(255, 179, 80, 0.7);
+  border-radius: 10px;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
